7b54c1299e75baa92dfc176f508d814a33f8eaf8
jackneer <clk500@hotmail.com>
2020-01-05 13:54:56 +0800

    mod #13

--- no13_roman_to_integer.py
+++ no13_roman_to_integer.py
@@ -1,6 +1,29 @@
 def roman_to_int(roman):
-    
-    return 0
+    roman_nums = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+
+    num = 0
+    temp_num = 0
+
+    for i in range(len(roman)):        
+        cur_num = roman_nums.get(roman[i:i+1])
+        prev_num = 0
+
+        if i > 0:
+            prev_num = roman_nums.get(roman[i-1:i])
+
+        if prev_num == 0 or prev_num == cur_num:
+            temp_num += cur_num
+        elif prev_num > cur_num:
+            num += temp_num
+            temp_num = cur_num
+        elif prev_num < cur_num:
+            num += cur_num - temp_num
+            temp_num = 0
+
+    if temp_num !=0:
+        num += temp_num
+
+    return num
     
 
 def main():
